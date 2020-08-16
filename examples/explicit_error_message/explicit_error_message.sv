8690d9dd9728c9163a22af2f66e55a40d847b404
Tudor Timi <tudortimi@users.noreply.github.com>
2020-08-16 19:33:47 +0200

    Fix years in headers

--- examples/explicit_error_message/explicit_error_message.sv
+++ examples/explicit_error_message/explicit_error_message.sv
@@ -1,4 +1,4 @@
-// Copyright 2015 Tudor Timisescu (verificationgentleman.com)
+// Copyright 2020 Tudor Timisescu (verificationgentleman.com)
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
