84fb6fc8e60696fc0b5a6bd36a03727f2f64b6f6
Tudor Timi <tudortimi@users.noreply.github.com>
2020-12-30 13:08:51 +0100

    Create a task for each example
    This makes it easier to see what examples are available and to start
    them.

--- buildSrc/src/main/groovy/RunExampleTask.groovy
+++ buildSrc/src/main/groovy/RunExampleTask.groovy
@@ -1,25 +1,13 @@
 import org.gradle.api.DefaultTask
-import org.gradle.api.InvalidUserDataException
+import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.TaskAction
-import org.gradle.api.tasks.options.Option
-import org.gradle.api.tasks.options.OptionValues
 
 class RunExampleTask extends DefaultTask {
-    @Option(option = 'name', description = 'name of the example to run')
+    @Input
     String exampleName
-
-    @OptionValues('name')
-    def List<String> getAvailableExamples() {
-	def exampleNames = []
-        project.file('.').eachDir {
-	        exampleNames << it.name
-        }
-        return exampleNames
-    }
 
     @TaskAction
     def run() {
-        checkExampleName()
         project.exec {
             executable 'xrun'
             args'-f', project.parent.genFullArgsFile.destination.get().asFile
@@ -27,12 +15,6 @@ class RunExampleTask extends DefaultTask {
             workingDir project.xrunDir
             project.mkdir workingDir
         }
-    }
-
-    def checkExampleName() {
-        def isLegal = exampleName in getAvailableExamples()
-        if (!isLegal)
-            throw new InvalidUserDataException('illegal example name')
     }
 
     def getExampleFiles() {
