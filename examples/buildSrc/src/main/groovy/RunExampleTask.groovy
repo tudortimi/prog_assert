28d2896dcc98528b7e8b80ffa6e7cc79232d54d7
Tudor Timi <tudortimi@users.noreply.github.com>
2023-01-15 16:38:21 +0200

    Make `xrunDir` a property of the task instead of the project
    We use `Property<Path>` because we don't want Gradle to consider the contents of the directory for up-to-date checks.

--- examples/buildSrc/src/main/groovy/RunExampleTask.groovy
+++ examples/buildSrc/src/main/groovy/RunExampleTask.groovy
@@ -1,11 +1,16 @@
+import java.nio.file.Path
+
 import org.gradle.api.DefaultTask
 import org.gradle.api.file.RegularFileProperty
-import org.gradle.api.tasks.InputFile
-import org.gradle.api.tasks.TaskAction
+import org.gradle.api.provider.Property
+import org.gradle.api.tasks.*
 
-class RunExampleTask extends DefaultTask {
+abstract class RunExampleTask extends DefaultTask {
     @InputFile
     RegularFileProperty fullXrunArgsFile
+
+    @Input
+    abstract Property<Path> getXrunDir()
 
     @TaskAction
     def run() {
@@ -13,7 +18,7 @@ class RunExampleTask extends DefaultTask {
             executable 'xrun'
             args '-f', fullXrunArgsFile.get()
             args '-top', project.name
-            workingDir project.xrunDir
+            workingDir xrunDir.get()
             project.mkdir workingDir
         }
     }
